4b90e353c4e721dd0b15f72d37f717a1470bddd0
oktay <yilmazoktay124@gmail.com>
2021-11-10 09:03:39 +0300

    example

--- example.php
+++ example.php
@@ -9,3 +9,10 @@ if (Cache::store('file')->get('aa.912') == null){
     $cache = Cache::store('file')->put('ipid.182.234.123',$value,42);
 }
 
+$response =  Cache::store('file')->put('Key','value',200);
+$response =  Cache::store('file')->put('aa.912','dasdasdas',2400);
+$response =  Cache::store('file')->get('aa.912','sdasd');
+Cache::store('file')->forget('aa.912');
+Cache::store('file')->flush();
+$response = Cache::store('file')->forever('name23','dasdasd');
+$response =  Cache::store('file')->get('name23');
